051cd2235911be699c46ab3c4d80ff6e60aeac8e
Konrad Maurer <kradjr@gmail.com>
2015-01-20 13:59:17 -0700

    Registers with twitter and added keys to oauth file.

--- omniauth.rb
+++ omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "API_KEY", "API_SECRET"
+  provider :twitter, "z1W3PKstumBfMX7RU3bQ9mij5", "rFDNdtMJGyynEegM6QB3pRTXb80mOw79UMcuZRIEUUCHdzIo4v"
 end
