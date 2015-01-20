9d29b0ccfadcf2a5794cf3b4b0fd6393d9d726a7
Konrad Maurer <kradjr@gmail.com>
2015-01-20 12:32:15 -0700

    Adds nested resources to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'staticpages#home'
+
+  resources :users do
+    resources :tweets, only: [:show, :index]
+  end
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
